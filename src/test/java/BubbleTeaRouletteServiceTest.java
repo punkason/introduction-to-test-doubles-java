70ebf793b19e97452a4e88217f9265272b1eaa11
punkason <punkason@gmail.com>
2022-01-18 12:43:16 +0000

    add activity 4 and 5

--- src/test/java/BubbleTeaRouletteServiceTest.java
+++ src/test/java/BubbleTeaRouletteServiceTest.java
@@ -4,6 +4,7 @@ import com.techreturners.bubbleteaordersystem.model.BubbleTeaTypeEnum;
 import org.junit.jupiter.api.Test;
 import testhelper.RandomStub;
 
+
 import static org.junit.Assert.assertEquals;
 
 public class BubbleTeaRouletteServiceTest {
@@ -11,6 +12,7 @@ public class BubbleTeaRouletteServiceTest {
     @Test
     public void shouldGenerateARandomBubbleTeaTypeWhenGetRandomBubbleTeaTypeIsCalled() {
 
+        //Jasmine Milk Tea
         //Arrange
         RandomStub randomStub = new RandomStub(BubbleTeaTypeEnum.JasmineMilkTea.ordinal());
         BubbleTeaRouletteService bubbleTeaRouletteService = new BubbleTeaRouletteService(randomStub);
@@ -21,6 +23,38 @@ public class BubbleTeaRouletteServiceTest {
 
         //Assert
         assertEquals(expectedResult, actualResult);
+    }
 
+    //Activity 4 - Peach Ice Tea
+    @Test
+    public void shouldGenerateARandomBubbleTeaTypeWhenGetRandomBubbleTeaTypeIsCalledWithPeachIceTea() {
+        //Arrange
+        RandomStub randomStub = new RandomStub(BubbleTeaTypeEnum.PeachIceTea.ordinal());
+        BubbleTeaRouletteService bubbleTeaRouletteService = new BubbleTeaRouletteService(randomStub);
+
+        //Act
+        var actualResult = bubbleTeaRouletteService.getRandomBubbleTeaType();
+        var expectedResult = BubbleTeaTypeEnum.PeachIceTea;
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Activity 5
+    @Test
+    public void parameterisedBubbleTeaTest() {
+        //Arrange
+        int i = 0;
+        for (BubbleTeaTypeEnum expectedResult : BubbleTeaTypeEnum.values()) {
+            RandomStub randomStub = new RandomStub(i);
+            BubbleTeaRouletteService bubbleTeaRouletteService = new BubbleTeaRouletteService(randomStub);
+
+            //Act
+            var actualResult = bubbleTeaRouletteService.getRandomBubbleTeaType();
+            //System.out.println(actualResult + " E: " + expectedResult);
+            //Assert
+            assertEquals(expectedResult, actualResult);
+            i++;
+        }
     }
 }
